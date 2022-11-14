0ef4d3e676d8ab2f7d93b2026cdaf61568294e6b
emmanuelcortes2 <109642972+emmanuelcortes2@users.noreply.github.com>
2022-11-14 15:38:19 -0600

    Add mobile menu style & fix script bug

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function toggle() {
   let x = document.getElementById("menu");
 
-  if(x.style.display === "block"){
+  if(x.style.display === "flex"){
       x.style.display = "none";
   }else {
-      x.style.display = "block";
+      x.style.display = "flex";
   }
 }
