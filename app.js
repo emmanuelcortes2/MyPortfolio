efac033aa2008b67165c861f392102514f05852e
emmanuelcortes2 <109642972+emmanuelcortes2@users.noreply.github.com>
2022-11-15 14:28:38 -0600

    Add styling and & graphics in js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const data = {
   badge1: 'HTML/CSS',
   badge2: 'Ruby on Rails',
   badge3: 'JavaScript',
+  graphic: './images/Rectangle21.svg',
   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
   btntext1: 'See live',
   btntext2: 'See source',
@@ -16,6 +17,7 @@ popUp.appendChild(close);
 close.src = './images/close.svg';
 close.height = '17';
 close.id = 'close-icon';
+close.onclick = pop;
 
 const title = document.createElement('h2');
 popUp.appendChild(title);
@@ -38,8 +40,8 @@ listC.textContent = (data.badge3);
 
 const image = document.createElement('img');
 popUp.appendChild(image);
-image.src = './images/Rectangle21.svg';
 image.id = 'img-popup';
+image.src = data.graphic;
 
 const paragraph = document.createElement('p');
 popUp.appendChild(paragraph);
@@ -47,19 +49,32 @@ paragraph.textContent = (data.text);
 
 const btn1 = document.createElement('button');
 popUp.appendChild(btn1);
+btn1.id = 'popBtn1';
 btn1.textContent = (data.btntext1);
 
 const btn2 = document.createElement('button');
 popUp.appendChild(btn2);
+btn2.id = 'popBtn2';
 btn2.textContent = (data.btntext2);
+
 
 /* eslint-disable */
 function toggle() {
   const x = document.getElementById('menu');
 
-  if (x.style.display === 'none') {
-    x.style.display = 'flex';
-  } else {
+  if (x.style.display === 'flex') {
     x.style.display = 'none';
+  } else {
+    x.style.display = 'flex';
+  }
+}
+
+function pop() {
+  const y = document.getElementById('popup');
+
+  if (y.style.display === 'flex') {
+    y.style.display = 'none';
+  } else {
+    y.style.display = 'flex';
   }
 }
