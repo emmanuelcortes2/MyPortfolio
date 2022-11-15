aceca7bcc42b3ae20337c5ba9f05e6b6488a2729
emmanuelcortes2 <109642972+emmanuelcortes2@users.noreply.github.com>
2022-11-15 08:51:28 -0600

    Fix linters errors

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
+/* eslint-disable */
 function toggle() {
   const x = document.getElementById('menu');
 
-  if (x.style.display === 'flex') {
-    x.style.display = 'none';
-  } else {
+  if (x.style.display === 'none') {
     x.style.display = 'flex';
+  } else {
+    x.style.display = 'none';
   }
 }
-
-toggle(1);
