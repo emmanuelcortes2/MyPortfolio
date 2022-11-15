6f9c85503086e6751e63d3f6f8951e1076e306f0
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2022-11-15 12:04:20 -0500

    Add dinamyc HTML elements.

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
+const sect = document.createDocumentFragment();
+const popUp = sect.appendChild(document.createElement('section'));
+popUp.textContent = 'hello world';
+
+document.body.appendChild(popUp);
+
+popUp.setAttribute('class', 'pop-up');
+
+const title = document.createElement('h2');
+popUp.appendChild(title);
+
+const badges = document.createElement('ul');
+popUp.appendChild(badges);
+
+const listA = document.createElement('li');
+badges.appendChild(listA);
+
+const listB = document.createElement('li');
+badges.appendChild(listB);
+
+const listC = document.createElement('li');
+badges.appendChild(listC);
+
+const image = document.createElement('img');
+popUp.appendChild(image);
+
+const paragraph = document.createElement('p');
+popUp.appendChild(paragraph);
+
+const btn1 = document.createElement('button');
+popUp.appendChild(btn1);
+
+const btn2 = document.createElement('button');
+popUp.appendChild(btn2);
+
+
 /* eslint-disable */
 function toggle() {
   const x = document.getElementById('menu');
