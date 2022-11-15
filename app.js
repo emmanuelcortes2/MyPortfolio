14cc27e60c651b559cd1f224d51bcbd091593301
emmanuelcortes2 <109642972+emmanuelcortes2@users.noreply.github.com>
2022-11-15 13:40:00 -0600

    Add pop up styling

--- app.js
+++ app.js
@@ -1,38 +1,57 @@
-const sect = document.createDocumentFragment();
-const popUp = sect.appendChild(document.createElement('section'));
-popUp.textContent = 'hello world';
+const data = {
+  title: 'Project name goes here',
+  badge1: 'HTML/CSS',
+  badge2: 'Ruby on Rails',
+  badge3: 'JavaScript',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+  btntext1: 'See live',
+  btntext2: 'See source',
+}
 
-document.body.appendChild(popUp);
+const popUp = document.getElementById('popup');
+console.log(popUp);
 
-popUp.setAttribute('class', 'pop-up');
+const close = document.createElement('img');
+popUp.appendChild(close);
+close.src = './images/close.svg';
+close.height = '17';
+close.id = 'close-icon';
 
 const title = document.createElement('h2');
 popUp.appendChild(title);
+title.textContent = (data.title);
 
 const badges = document.createElement('ul');
 popUp.appendChild(badges);
 
 const listA = document.createElement('li');
 badges.appendChild(listA);
+listA.textContent = (data.badge1);
 
 const listB = document.createElement('li');
 badges.appendChild(listB);
+listB.textContent = (data.badge2);
 
 const listC = document.createElement('li');
 badges.appendChild(listC);
+listC.textContent = (data.badge3);
 
 const image = document.createElement('img');
 popUp.appendChild(image);
+image.src = './images/Rectangle21.svg';
+image.id = 'img-popup';
 
 const paragraph = document.createElement('p');
 popUp.appendChild(paragraph);
+paragraph.textContent = (data.text);
 
 const btn1 = document.createElement('button');
 popUp.appendChild(btn1);
+btn1.textContent = (data.btntext1);
 
 const btn2 = document.createElement('button');
 popUp.appendChild(btn2);
-
+btn2.textContent = (data.btntext2);
 
 /* eslint-disable */
 function toggle() {
