e1618b8586d5e15cc5959dd0aec59cca8e2365ad
emmanuelcortes2 <109642972+emmanuelcortes2@users.noreply.github.com>
2022-11-16 15:42:29 -0600

    Add objects & change style mobile & desktop popup

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function pop() {
   const y = document.getElementById('popup');
 
-  if (y.style.display === 'flex') {
-    y.style.display = 'none';
-  } else {
+  if (y.style.display === 'none') {
     y.style.display = 'flex';
+  } else {
+    y.style.display = 'none';
   }
 }
 
@@ -18,64 +18,127 @@ function popDesk() {
   }
 }
 
-const data = {
-  title: 'Project name goes here',
-  badge1: 'HTML/CSS',
-  badge2: 'Ruby on Rails',
-  badge3: 'JavaScript',
-  graphic: './images/Rectangle21.svg',
-  deskgraphic: './images/Group 104.svg',
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
-  btntext1: 'See live',
-  btntext2: 'See source',
-};
+const data = [
+  {
+    title: 'Project name goes here',
+    badge1: 'HTML/CSS',
+    badge2: 'Ruby on Rails',
+    badge3: 'JavaScript',
+    graphic: './images/Rectangle21.svg',
+    deskgraphic: './images/Group 104.svg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    btntext1: 'See live',
+    btntext2: 'See source'
+  },
+  {
+    title: 'Project name goes here',
+    badge1: 'HTML/CSS',
+    badge2: 'Ruby on Rails',
+    badge3: 'JavaScript',
+    graphic: './images/Image2.svg',
+    deskgraphic: './images/Group 104.svg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    btntext1: 'See live',
+    btntext2: 'See source'
+  },
+  {
+    title: 'Project name goes here',
+    badge1: 'HTML/CSS',
+    badge2: 'Ruby on Rails',
+    badge3: 'JavaScript',
+    graphic: './images/Image3.svg',
+    deskgraphic: './images/Group 104.svg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    btntext1: 'See live',
+    btntext2: 'See source'
+  },
+  {
+    title: 'Project name goes here',
+    badge1: 'HTML/CSS',
+    badge2: 'Ruby on Rails',
+    badge3: 'JavaScript',
+    graphic: './images/Image4.svg',
+    deskgraphic: './images/Group 104.svg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    btntext1: 'See live',
+    btntext2: 'See source'
+  },
+  {
+    title: 'Project name goes here',
+    badge1: 'HTML/CSS',
+    badge2: 'Ruby on Rails',
+    badge3: 'JavaScript',
+    graphic: './images/Image5.svg',
+    deskgraphic: './images/Group 104.svg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    btntext1: 'See live',
+    btntext2: 'See source'
+  },
+  {
+    title: 'Project name goes here',
+    badge1: 'HTML/CSS',
+    badge2: 'Ruby on Rails',
+    badge3: 'JavaScript',
+    graphic: './images/Image6.svg',
+    deskgraphic: './images/Group 104.svg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    btntext1: 'See live',
+    btntext2: 'See source'
+  }
+]
+//mobile popup
 
-const popUp = document.getElementById('popup');
+function showProject(a) {
 
-const close = document.createElement('img');
-popUp.appendChild(close);
-close.src = './images/close.svg';
-close.height = '17';
-close.id = 'close-icon';
-close.onclick = pop;
+  const popUp = document.getElementById('popup');
 
-const title = document.createElement('h2');
-popUp.appendChild(title);
-title.textContent = (data.title);
+  const close = document.createElement('img');
+  popUp.appendChild(close);
+  close.src = './images/close.svg';
+  close.height = '17';
+  close.id = 'close-icon';
+  close.onclick = pop;
 
-const badges = document.createElement('ul');
-popUp.appendChild(badges);
+  const title = document.createElement('h2');
+  popUp.appendChild(title);
+  title.textContent = (data[a].title);
 
-const listA = document.createElement('li');
-badges.appendChild(listA);
-listA.textContent = (data.badge1);
+  const badges = document.createElement('ul');
+  popUp.appendChild(badges);
 
-const listB = document.createElement('li');
-badges.appendChild(listB);
-listB.textContent = (data.badge2);
+  const listA = document.createElement('li');
+  badges.appendChild(listA);
+  listA.textContent = (data[a].badge1);
 
-const listC = document.createElement('li');
-badges.appendChild(listC);
-listC.textContent = (data.badge3);
+  const listB = document.createElement('li');
+  badges.appendChild(listB);
+  listB.textContent = (data[a].badge2);
 
-const image = document.createElement('img');
-popUp.appendChild(image);
-image.id = 'img-popup';
-image.src = data.graphic;
+  const listC = document.createElement('li');
+  badges.appendChild(listC);
+  listC.textContent = (data[a].badge3);
 
-const paragraph = document.createElement('p');
-popUp.appendChild(paragraph);
-paragraph.textContent = (data.text);
+  const image = document.createElement('img');
+  popUp.appendChild(image);
+  image.id = 'img-popup';
+  image.src = data[a].graphic;
 
-const btn1 = document.createElement('button');
-popUp.appendChild(btn1);
-btn1.id = 'popBtn1';
-btn1.textContent = (data.btntext1);
+  const paragraph = document.createElement('p');
+  popUp.appendChild(paragraph);
+  paragraph.textContent = (data[a].text);
 
-const btn2 = document.createElement('button');
-popUp.appendChild(btn2);
-btn2.id = 'popBtn2';
-btn2.textContent = (data.btntext2);
+  const btn1 = document.createElement('button');
+  popUp.appendChild(btn1);
+  btn1.id = 'popBtn1';
+  btn1.textContent = (data[a].btntext1);
+
+  const btn2 = document.createElement('button');
+  popUp.appendChild(btn2);
+  btn2.id = 'popBtn2';
+  btn2.textContent = (data[a].btntext2);
+}
+
+// desktop popup
 
 const deskPop = document.getElementById('desk-popup');
 deskPop.id = 'desk-popup';
@@ -89,31 +152,31 @@ closedesk.onclick = popDesk;
 
 const titledesk = document.createElement('h2');
 deskPop.appendChild(titledesk);
-titledesk.textContent = (data.title);
+titledesk.textContent = (data[1].title);
 
 const badgesdesk = document.createElement('ul');
 deskPop.appendChild(badgesdesk);
 
 const listAdesk = document.createElement('li');
 badgesdesk.appendChild(listAdesk);
-listAdesk.textContent = (data.badge1);
+listAdesk.textContent = (data[1].badge1);
 
 const listBdesk = document.createElement('li');
 badgesdesk.appendChild(listBdesk);
-listBdesk.textContent = (data.badge2);
+listBdesk.textContent = (data[1].badge2);
 
 const listCdesk = document.createElement('li');
 badgesdesk.appendChild(listCdesk);
-listCdesk.textContent = (data.badge3);
+listCdesk.textContent = (data[1].badge3);
 
 const deskGraph = document.createElement('img');
 deskPop.appendChild(deskGraph);
-deskGraph.src = (data.deskgraphic);
+deskGraph.src = (data[1].deskgraphic);
 deskGraph.id = 'desk-graph';
 
 const paragraphDesk = document.createElement('p');
 deskPop.appendChild(paragraphDesk);
-paragraphDesk.textContent = (data.text);
+paragraphDesk.textContent = (data[1].text);
 
 const btndiv = document.createElement('div');
 deskPop.appendChild(btndiv);
@@ -122,12 +185,12 @@ btndiv.id = 'btndiv';
 const btndesk1 = document.createElement('button');
 btndiv.appendChild(btndesk1);
 btndesk1.id = 'popBtn1-desk';
-btndesk1.textContent = (data.btntext1);
+btndesk1.textContent = (data[1].btntext1);
 
 const btndesk2 = document.createElement('button');
 btndiv.appendChild(btndesk2);
 btndesk2.id = 'popBtn2-desk';
-btndesk2.textContent = (data.btntext2);
+btndesk2.textContent = (data[1].btntext2);
 
 const fwdBck = document.createElement('div');
 deskPop.appendChild(fwdBck);
